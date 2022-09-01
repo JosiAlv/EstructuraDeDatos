f7d91b06db39109e75a1f0aabb705c73fd2d1566
JosiAlv <88679712+JosiAlv@users.noreply.github.com>
2022-08-31 22:11:33 -0400

    Add files via upload

--- Campeonato/Clases/Main3.java
+++ Campeonato/Clases/Main3.java
@@ -8,31 +8,49 @@ public class Main3 {
 
         System.out.println("INGRESE DATOS DE JUGADORES");
         String nombreCompleto, apellidos, ci;
-        int edad, i, nJugadores = 3;
+        int edad, i, nJugadores = 1;
 
-        Jugador[] jugadores = new Jugador[nJugadores];
+        System.out.println("INGRESE DATOS DE EQUIPOS");
+            String nombreEquipo, categoria;
+            int j, nEquipos = 2;
 
-        for (i = 0; i < nJugadores; i = i + 1) {
-            System.out.println("Ingrese el nombre del jugador " + (i+1) + ": ");
-            nombreCompleto = leer.next();
-            System.out.println("Ingrese el apellidos del jugador " + (i+1) + ": ");
-            apellidos = leer.next();
-            System.out.println("Ingrese el CI del jugador " + (i+1) + ": ");
-            ci = leer.next();
-            System.out.println("Ingrese edad del jugador " + (i+1) + ": ");
-            edad = leer.nextInt();
+            Equipo[] equipos = new Equipo[nEquipos];
 
-            Jugador jug = new Jugador();
-            jug.setNombreCompleto(nombreCompleto);
-            jug.setApellidos(apellidos);
-            jug.setCI(ci);
-            jug.setEdad(edad);
-            jug.mostrarJugador();
+            for (j = 0; j < nEquipos; j = j + 1) {
+                System.out.println("Ingrese el nombre del equipo " + (j + 1) + ": ");
+                nombreEquipo = leer.next();
+                System.out.println("Ingrese la categoria del equipo " + (j + 1) + ": ");
+                categoria = leer.next();
+                Jugador[] jugadores = new Jugador[nJugadores];
+                for (i = 0; i < nJugadores; i = i + 1) {
+                    System.out.println("Ingrese el nombre del jugador " + (i + 1) + ": ");
+                    nombreCompleto = leer.next();
+                    System.out.println("Ingrese el apellidos del jugador " + (i + 1) + ": ");
+                    apellidos = leer.next();
+                    System.out.println("Ingrese el CI del jugador " + (i + 1) + ": ");
+                    ci = leer.next();
+                    System.out.println("Ingrese edad del jugador " + (i + 1) + ": ");
+                    edad = leer.nextInt();
 
-            jugadores [i] = jug;
-        }
+                    Jugador jug = new Jugador();
+                    jug.setNombreCompleto(nombreCompleto);
+                    jug.setApellidos(apellidos);
+                    jug.setCI(ci);
+                    jug.setEdad(edad);
+                  //  Pijamada con el inge
 
-        Equipo eq1 = new Equipo("Club Saltena", "Varones", jugadores);
-        eq1.mostrarEquipo();
+                    jugadores[i] = jug;
+                }
+
+                Equipo Eq = new Equipo();
+                Eq.setNombreEquipo(nombreEquipo);
+                Eq.setCategoria(categoria);
+                Eq.setJugadores(jugadores);
+              //
+                equipos[j] = Eq;
+            }
+        Campeonato Camp1 = new Campeonato("UNIFRANZITOS", equipos);
+        Camp1.mostrarCampeonato();
+
     }
 }
