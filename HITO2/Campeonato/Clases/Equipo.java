57264ae2c00325136a054fb182dc86f232fbe598
JosiAlv <88679712+JosiAlv@users.noreply.github.com>
2022-08-31 23:41:50 -0400

    Add files via upload

---
+++ HITO2/Campeonato/Clases/Equipo.java
@@ -0,0 +1,47 @@
+package Campeonato.Clases;
+
+import java.util.SplittableRandom;
+
+public class Equipo {
+    private String nombreEquipo;
+    private String categoria;
+    private Jugador[] jugadores;
+
+    public Equipo(String nombreEquipo, String categoria, Jugador[] jugadores) {
+        this.nombreEquipo = nombreEquipo;
+        this.categoria = categoria;
+        this.jugadores = jugadores;
+    }
+    public Equipo () {}
+    public String getNombreEquipo() {
+        return this.nombreEquipo;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+    public Jugador[] getJugadores() {
+        return this.jugadores;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setJugadores(Jugador[] jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public void mostrarEquipo() {
+        System.out.println("\nMOSTRANDO DATOS DEL EQUIPO");
+        System.out.println("Nombre Equipo: " + this.getNombreEquipo());
+        System.out.println("Categoria Equipo: " + this.getCategoria());
+        for (int i=0; i < getJugadores().length; i++) {
+            this.getJugadores()[i].mostrarJugador();
+        }
+    }
+}
